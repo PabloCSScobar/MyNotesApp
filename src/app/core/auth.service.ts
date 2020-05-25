04a2be3f30492ff8b57440507de4b6738b69240f
PawelGajo <pudzian12312@gmail.com>
2020-05-25 20:08:17 +0200

    add login func in authService.

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment} from '../../environments/environment'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
-  constructor() { }
+export class AuthService {
+  constructor(private http: HttpClient) { }
+  login(loginData) {
+    this.http.post(`${environment.apiUrl}/auth/`, loginData).subscribe( token => console.log(token));
+  }
 }
