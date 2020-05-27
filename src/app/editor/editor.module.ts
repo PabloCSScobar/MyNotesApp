aac5d41038b41db875ab0f1b7458d0130f4ea6c3
PawelGajo <pudzian12312@gmail.com>
2020-05-27 09:00:20 +0200

    add reactiveforms to editor.module

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -5,12 +5,16 @@ import { NavComponent } from './nav/nav.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NoteComponent } from './note/note.component';
 import { EditorRoutingModule } from './editor-routing.module';
+import { NewNoteComponent } from './new-note/new-note.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [MainComponent, NavComponent, SidebarComponent, NoteComponent],
+  declarations: [MainComponent, NavComponent, SidebarComponent, NoteComponent, NewNoteComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     EditorRoutingModule
   ]
