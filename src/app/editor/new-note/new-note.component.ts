ab2d81783560a78362bbcf463e4ab2757ae965b9
PawelGajo <pudzian12312@gmail.com>
2020-05-29 08:20:33 +0200

    navigate to note after adding

--- src/app/editor/new-note/new-note.component.ts
+++ src/app/editor/new-note/new-note.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NotesService } from '../notes.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-note',
   templateUrl: './new-note.component.html',
@@ -11,10 +12,12 @@ export class NewNoteComponent implements OnInit {
     title: new FormControl('', Validators.required),
     body: new FormControl('', Validators.required)
   });
-  constructor(private notesService: NotesService) { }
+  constructor(private notesService: NotesService, private router: Router) { }
 
   addNote() {
-    this.notesService.addNote(this.newNoteForm.value);
+    this.notesService.addNote(this.newNoteForm.value).subscribe( note =>
+      this.router.navigate([`notes/edit/${note.id}`])
+    )
   }
   ngOnInit(): void {
   }
