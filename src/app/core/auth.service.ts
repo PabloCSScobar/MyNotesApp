bae30fd5ac182a80af3e002ecf726df15c61ab88
Gajo Pawel <Pawel.Gajo@snt.pl>
2020-06-02 23:39:31 +0200

    missing semicolons

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment} from '../../environments/environment'
+import { environment} from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { tap, catchError } from 'rxjs/operators';
@@ -26,7 +26,7 @@ export class AuthService {
     return this.http.post<any>(`${environment.apiUrl}/auth/`, loginData).pipe(
       tap( data => this.setToken(data.token)),
       tap( data => this.router.navigate(['notes'])),
-    )
+    );
   }
   register(registerData) {
     this.http.post(`${environment.apiUrl}/api/users/`, registerData);
