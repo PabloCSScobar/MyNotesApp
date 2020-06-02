90b188c3c5e5d0f90a8e74169551da54836554a1
Gajo Pawel <Pawel.Gajo@snt.pl>
2020-06-02 23:42:51 +0200

    add error messages to auth forms. change layout

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth.service';
+import { CustomValidators } from 'src/app/shared/custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -9,17 +10,30 @@ import { AuthService } from 'src/app/core/auth.service';
 })
 export class RegisterComponent implements OnInit {
   registerForm = new FormGroup({
-    first_name: new FormControl('', Validators.required),
-    last_name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
-  });
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      CustomValidators.email
+    ]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      CustomValidators.passwordStrength
+    ]),
+    confirm_password: new FormControl('', Validators.required)
+  }, {
+    validators: [CustomValidators.passwordMatch]
+  } );
   constructor(private authService: AuthService) { }
 
 
   register() {
-    if(this.registerForm.status === "VALID") {
+    console.log(this.registerForm);
+    if (this.registerForm.status === 'VALID') {
       this.authService.register(this.registerForm.value);
     }
   }
