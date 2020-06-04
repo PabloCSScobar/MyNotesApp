54f07b3e1cdaf7a79c45daff6853a12cdfc2b0a2
Gajo Pawel <Pawel.Gajo@snt.pl>
2020-06-04 10:28:08 +0200

    add resize func

--- src/app/editor/new-note/new-note.component.ts
+++ src/app/editor/new-note/new-note.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NotesService } from '../notes.service';
 import { Router } from '@angular/router';
+declare var $: any;
+
 @Component({
   selector: 'app-new-note',
   templateUrl: './new-note.component.html',
@@ -14,12 +16,22 @@ export class NewNoteComponent implements OnInit {
   });
   constructor(private notesService: NotesService, private router: Router) { }
 
+
+
   addNote() {
     this.notesService.addNote(this.newNoteForm.value).subscribe( note =>
       this.router.navigate([`notes/edit/${note.id}`])
-    )
+    );
   }
   ngOnInit(): void {
+    this.resizeTextarea();
   }
-
+  resizeTextarea() {
+    $('textarea').on({input() {
+      const totalHeight = $(this).prop('scrollHeight') - parseInt($(this).css('padding-top'), 10) - parseInt($(this).css('padding-bottom'), 10);
+      $(this).css({height: totalHeight});
+  }
+  });
+  }
 }
+
