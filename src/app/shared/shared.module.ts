1094c1d836c7753d5d86d490a5e754056a02532b
PawelGajo <gpawel1997@gmail.com>
2020-06-04 22:28:17 +0200

    create pipe for cuting too long note titles.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CutStringPipe } from './pipes/cut-string.pipe';
 
 @NgModule({
-  declarations: [ErrorMessageComponent],
+  declarations: [ErrorMessageComponent, CutStringPipe],
   imports: [
     FontAwesomeModule,
     CommonModule
   ],
   exports: [
+    CutStringPipe,
     ErrorMessageComponent,
     FontAwesomeModule
   ]
