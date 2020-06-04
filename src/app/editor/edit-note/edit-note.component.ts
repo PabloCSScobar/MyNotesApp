c0df8eb89c5dfb5a9a4604f359279d3a1d0f40bc
Gajo Pawel <Pawel.Gajo@snt.pl>
2020-06-04 16:48:29 +0200

    add quill and styles to editNote

--- src/app/editor/edit-note/edit-note.component.ts
+++ src/app/editor/edit-note/edit-note.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NotesService } from '../notes.service';
+import { faSave, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+declare var $: any;
 
 @Component({
   selector: 'app-edit-note',
@@ -9,6 +11,8 @@ import { NotesService } from '../notes.service';
   styleUrls: ['./edit-note.component.css']
 })
 export class EditNoteComponent implements OnInit {
+  saveIcon = faSave;
+  trashIcon = faTrashAlt;
   note = null;
   editNoteForm = new FormGroup({
     title: new FormControl('', Validators.required),
@@ -19,30 +23,40 @@ export class EditNoteComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.getNote(params.get("id"));
-      if(this.note) {
+      this.getNote(parseInt(params.get('id'), 10));
+      if (this.note) {
         this.editNoteForm = this.fillForm();
       }
-    })
+    });
+    this.resizeTextarea();
   }
   editNote() {
     this.notesService.updateNote(this.note.id, this.editNoteForm.value);
   }
   getNote(id) {
-    this.note = this.notesService.notesSubject.getValue().filter( x => x.id == id)[0];
-    if(!this.note) {
+    this.note = this.notesService.notesSubject.getValue().filter( x => x.id === id)[0];
+    console.log( this.notesService.notesSubject.getValue());
+    if (!this.note) {
       this.router.navigate(['notes']);
     }
   }
   deleteNote() {
     this.notesService.deleteNote(this.note.id).subscribe( () =>
       this.router.navigate(['notes'])
-    )
+    );
   }
   fillForm() {
     return new FormGroup({
       title: new FormControl(this.note.title, Validators.required),
       body: new FormControl(this.note.body, Validators.required)
     });
+  }
+
+  resizeTextarea() {
+    $('textarea').on({input() {
+      const totalHeight = $(this).prop('scrollHeight') - parseInt($(this).css('padding-top'), 10) - parseInt($(this).css('padding-bottom'), 10) ;
+      $(this).css({height: totalHeight});
+  }
+  });
   }
 }
