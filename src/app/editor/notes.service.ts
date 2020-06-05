f02f47fc5e25879874cf3d377d992b2320116df8
PawelGajo <gpawel1997@gmail.com>
2020-06-05 11:33:59 +0200

    remove headers (moved to interceptor)

--- src/app/editor/notes.service.ts
+++ src/app/editor/notes.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment} from '../../environments/environment'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service';
 import { BehaviorSubject } from 'rxjs';
 import { tap, map } from 'rxjs/operators';
+import { AuthService } from '../core/auth.service';
 
 
 @Injectable({
@@ -15,23 +15,23 @@ export class NotesService {
 
   constructor(
     private http: HttpClient,
-    private cookieService: CookieService,
+    private auth: AuthService
     ) { }
   
   getNotes() {
-    return this.http.get(`${environment.apiUrl}/api/notes/`, {headers: this.getHeaders()}).pipe(
+    return this.http.get(`${environment.apiUrl}/api/notes/`).pipe(
       tap( notes => this.notesSubject.next(notes))
     );
   }
 
   addNote(note) {
-    return this.http.post<any>(`${environment.apiUrl}/api/notes/`, note, {headers: this.getHeaders()}).pipe(
+    return this.http.post<any>(`${environment.apiUrl}/api/notes/`, note).pipe(
       map(res => res.result),
       tap( res => this.notesSubject.next([...this.notesSubject.getValue(), res]))
     )
   }
   deleteNote(id) {
-    return this.http.delete(`${environment.apiUrl}/api/notes/${id}/`, {headers: this.getHeaders()}).pipe(
+    return this.http.delete(`${environment.apiUrl}/api/notes/${id}/`).pipe(
       tap(
         () => this.notesSubject.next(this.notesSubject.getValue().filter( x => x.id != id))
     )
@@ -39,7 +39,7 @@ export class NotesService {
   }
 
   updateNote(id, note) {
-    this.http.put(`${environment.apiUrl}/api/notes/${id}/`, note, {headers: this.getHeaders()}).subscribe( response => {
+    this.http.put(`${environment.apiUrl}/api/notes/${id}/`, note).subscribe( response => {
       console.log(response);
       this.notesSubject.next( this.notesSubject.getValue().map(
         x=> {
@@ -50,11 +50,5 @@ export class NotesService {
         }
       ));
     });
-  }
-  getHeaders() {
-    return new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Token fd8a99605d9fe581e9a6a65f0b9221aaffeebb86`
-    })
   }
 }
