dc465b4e699bd88c101c39e616ebfbbaee08ca20
PawelGajo <gpawel1997@gmail.com>
2020-06-05 13:46:30 +0200

    add username img and logout.

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -22,13 +22,21 @@ export class AuthService {
   getToken() {
     return this.cookieService.get('Token');
   }
+  getUsername() {
+    return this.http.get<any>(`${environment.apiUrl}/api/users/get_user_info/`);
+  }
   login(loginData) {
     return this.http.post<any>(`${environment.apiUrl}/auth/`, loginData).pipe(
       tap( data => this.setToken(data.token)),
       tap( data => this.router.navigate(['notes'])),
     );
   }
+  logout() {
+    this.cookieService.delete('Token');
+    console.log(this.cookieService.get('Token'));
+    this.router.navigate(['auth/login']);
+  }
   register(registerData) {
-    this.http.post(`${environment.apiUrl}/api/users/`, registerData);
+    this.http.post(`${environment.apiUrl}/api/users/`, registerData).subscribe(res=>console.log(res), err =>console.log(err));
   }
 }
--- src/app/editor/nav/nav.component.ts
+++ src/app/editor/nav/nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/auth.service';
+import { tap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  signOutIcon = faSignOutAlt;
+  username: Observable<any> = null;
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.getUsername();
+  }
+  getUsername() {
+    this.username = this.auth.getUsername().pipe(
+      map( res => res.user)
+    )
+  }
+  logout() {
+    this.auth.logout();
+    console.log('logout');
   }
 
 }
